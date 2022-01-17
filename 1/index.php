c7c16313a84fed94614679db63502a4da47f7dd6
wahyuramadan123 <97865054+wahyuramadan123@users.noreply.github.com>
2022-01-17 12:21:23 +0700

    Create index.php

---
+++ 1/index.php
@@ -0,0 +1,21 @@
+<span style="font-wight: bolder;">
+NIM = 1922500140<br>
+Nama = Wahyu ramadhan<br>
+Kelompok = SI5B<br>
+</span>
+
+pada pertemuan 1 tanggal 11 oktober 2021, saya dapat menyimpulkan antara lain:<br>
+<ol>
+  <li>tata tertib kuliah, menjadi anak yaang baik</li>
+  <li>Wordpress zip do download dari web wordpress.org</li>
+  <li>WOrdpress zip perlu di extrack dan akan diproleh folder wordpress</li>
+  <li>bagaiimana cara menginstal wordpress</li>
+  <li>membuat database</li>
+ <li>menginsatal aplikasinya</li>
+ <li>cara mengekstrak wordpress dan memasukan kedalam folder hatdocs</li>
+ <li>aplikasi dan database harus dibackup</li>
+ <li>kita harus tahu import dan export database</li>
+ <li>mengetahui perbedaan wordpress.com dan wordpress.org</li>
+ <li>wordpress.com : blog yang sudah jadi</li>
+ <li>wordpress.org : dapat meng-host situs web atau blog Anda sendiri.</li>
+ </ol>
